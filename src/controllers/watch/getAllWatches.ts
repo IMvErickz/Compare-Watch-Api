2159c317d90490b97171826b86b57892db9cd783
IMvErickz <erickspy2003@gmail.com>
2024-05-08 22:56:51 -0300

    fix: return brand information

--- src/controllers/watch/getAllWatches.ts
+++ src/controllers/watch/getAllWatches.ts
@@ -3,7 +3,18 @@ import { prisma } from "../../lib/prisma";
 
 export async function getAllWatches(app: FastifyInstance) {
     app.get('/watch', async () => {
-        const watches = await prisma.watch.findMany()
+        const watches = await prisma.watch.findMany({
+            include: {
+                Brand: {
+                    select: {
+                        id: true,
+                        description: true,
+                        name: true,
+                        watch: true,
+                    }
+                }
+            }
+        })
 
         return watches
     })
