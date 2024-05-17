8074a65445af495694a4fa5afe12e9bdca7fa80d
Ricardo Sobral <riki.sobral@gmail.com>
2024-05-17 17:51:13 -0300

    auth

--- src/controllers/user/auth.ts
+++ src/controllers/user/auth.ts
@@ -1,38 +1,47 @@
-import { FastifyInstance } from "fastify"
-import { z } from "zod"
-import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../../lib/prisma";
 import { authenticate } from "../../plugins/authenticate";
-import bcrypt from 'bcrypt'
-
+import bcrypt from "bcrypt";
 
 export async function auth(fastify: FastifyInstance) {
-  fastify.post('/auth', async (request, reply) => {
+  fastify.post("/auth", async (request, reply) => {
     const createUserBody = z.object({
-      email: z.string().email('Email ou senha invalidos'),
-      password: z.string()
-    })
+      email: z.string().email("Email ou senha invalidos"),
+      password: z.string(),
+    });
 
-    const { email, password } = createUserBody.parse(request.body)
+    const { email, password } = createUserBody.parse(request.body);
 
-    const user = await prisma.user.findUniqueOrThrow({
+    const user = await prisma.user.findUnique({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
 
-    const hash = bcrypt.compareSync(password, user.password)
-
-    if (hash) {
-      const token = fastify.jwt.sign({
-        name: user.name,
-        email: user.email
-      }, {
-        sub: user.id,
-        expiresIn: '7 days'
-      })
-
-      return reply.status(200).send(`Bearer ${token}`)
+    if (!user) {
+      return reply.status(400).send("Email or password invalid!");
     }
 
-  })
+    const hash = bcrypt.compareSync(password, user.password);
+
+    if (hash) {
+      const token = fastify.jwt.sign(
+        {
+          name: user.name,
+          email: user.email,
+        },
+        {
+          sub: user.id,
+          expiresIn: "7 days",
+        }
+      );
+
+      return reply.status(200).send({
+        email: user.email,
+        name: user.name,
+        accessToken: token,
+      });
+    }
+  });
 }
